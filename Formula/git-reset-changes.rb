884a2f739a69b64be3cc21c27e5c538a192751d6
kennysliding <kennychiu.v@gmail.com>
2026-05-27 13:36:07 +0800

    Update git-reset-changes

--- Formula/git-reset-changes.rb
+++ Formula/git-reset-changes.rb
@@ -3,11 +3,11 @@ class GitResetChanges < Formula
   homepage "https://github.com/neologism-research/git-reset-changes"
   
   # Points to the stable release tarball of your core repository
-  url "https://github.com/neologism-research/git-reset-changes/archive/refs/tags/v0.1.0.tar.gz"
-  version "0.1.0"
+  url "https://github.com/neologism-research/git-reset-changes/archive/refs/tags/v0.2.0.tar.gz"
+  version "0.2.0"
   
-  # The SHA256 checksum of your v0.1.0.tar.gz file to ensure security.
-  sha256 "f8c5fb5c7a3e90e1ee00fac05c7410bfde5a26f05522c4f6f89380e8e8da3fc3"
+  # The SHA256 checksum of your v0.2.0.tar.gz file to ensure security.
+  sha256 "2e02e7c66fd528de2f478bb19047d448d5f66750ed19e3fb5993750aeca6cfa0"
 
   # Tells Homebrew to automatically install fzf if the user doesn't have it
   depends_on "fzf"
